1b7457bb2e3bbe5f34974031edccc99c768039a4
Fabiano Brito <fabiano.brito@wolt.com>
2020-10-01 21:29:14 +0300

    Limit movement of the paddle

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Paddle : MonoBehaviour
 {
     [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
 
     void Start()
     {
@@ -14,7 +16,8 @@ public class Paddle : MonoBehaviour
     void Update()
     {
         float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
-        Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
         transform.position = paddlePos;
     }
 }
