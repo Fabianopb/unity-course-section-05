f515ebda29b794fef18b6aedd17ee1d84634e046
Fabiano Brito <fabiano.brito@wolt.com>
2020-10-05 23:51:09 +0300

    Create Level object and script to keep track of the amount of blocks

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -7,9 +7,18 @@ public class Block : MonoBehaviour
 
     [SerializeField] AudioClip breakSound;
 
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.AddBlock();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);   
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        level.SubtractBlock();
         Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] int blockCount = 0;
+
+    public void AddBlock()
+    {
+        blockCount++;
+    }
+
+    public void SubtractBlock()
+    {
+        blockCount--;
+    }
+}
